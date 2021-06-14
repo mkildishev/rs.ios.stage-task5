887f492363515268be8db32619c8844abbfd8ed9
Maksim Kildishev <mkildishev@yandex.ru>
2021-06-14 21:04:12 +0300

    feat: add first task and partially second

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,14 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        guard let maxPrice = prices.max() else {
+            return 0
+        }
+        let maxPriceIdx = prices.firstIndex(of: maxPrice)!
+        let priceSlice = prices[0...maxPriceIdx]
+        let profit = priceSlice.reduce(0) {
+            $0 + (maxPrice - $1)
+        }
+        return profit + countProfit(prices: Array(prices.dropFirst(maxPriceIdx + 1)))
     }
 }
--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -1,8 +1,58 @@
 import UIKit
 
+
+extension Character {
+    var isOpenBracket: Bool {
+        self == "["
+    }
+    var isCloseBracket: Bool {
+        self == "]"
+    }
+}
+
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+//        guard let startIdx = message.firstIndex(where: {$0.isNumber})  else {
+//            return message;
+//        }
+//        let newMessage = String(Array(message)[(startIdx+1)...])
+        return buildAndRepeat(message, 1)
+    }
+    
+    func buildAndRepeat(_ message: String, _ count: Int) -> String {
+        var needIgnoreCloseBrackets = false
+        var numberAgain = false
+        var result = ""
+        var finalResult = ""
+        var prevElem:Character = "z"
+        for (num, ch) in message.enumerated() {
+            if ch.isLetter {
+                result.append(ch)
+            }
+            if ch.isCloseBracket && !needIgnoreCloseBrackets {
+                result = String(repeating: result, count: count)
+                needIgnoreCloseBrackets = true
+            }
+            if ch.isNumber {
+                let newMessage = String(Array(message)[(num+1)...])
+                numberAgain = prevElem.isOpenBracket
+                finalResult = buildAndRepeat(String(newMessage), numberAgain ? (count * Int(String(ch))!) : Int(String(ch))!)
+                if needIgnoreCloseBrackets {
+                    finalResult = result + finalResult
+                } else if let _ = message.lastIndex(where: {$0.isCloseBracket}) {
+                    if (message.last?.isCloseBracket ?? false) {
+                        finalResult = String(repeating: result + finalResult, count: count)
+                    } else {
+                        finalResult = String(repeating: result, count: count) + finalResult
+                    }
+                }
+                break
+            }
+            prevElem = ch
+        }
+        return finalResult == "" ? result : finalResult
     }
 }
+
+
